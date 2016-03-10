a7c5d8db98abd7d5db5a19a25905c3527b770b48
pacotoh <pacotoh@localhost.localdomain>
2016-03-11 00:46:50 +0100

    don't work loading after closing with save, overload the images but works if don't close the program.

--- feature_matching.py
+++ feature_matching.py
@@ -10,11 +10,15 @@ def play(dev=0):
     cap = cv.VideoCapture(dev)
     model_index = 0
     models_path = 'images/feature_matching/models/'
-    tests_path = 'images/feature_matching/tests/'
-    models = []
-    tests = [] # list of the created image models
+    models = [] # list of the created image models
     save = False
     load = False
+    matching = False
+
+    #akaze = cv.AKAZE_create()
+    #bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck = True)
+    method = cv.xfeatures2d.SIFT_create()
+    bf = cv.BFMatcher()
 
     while(True):
         key = cv.waitKey(1) & 0xFF
@@ -22,17 +26,18 @@ def play(dev=0):
 
         if os.path.exists(models_path + 'save.pkl'):
             load = True
+            matching = True
+            models, model_index = ut.load_session(models_path)
 
         # load the number of models and the models
         if key == ord('l') and load:
             save = True
             models, ind = ut.load_session(models_path)
-            print(ind)
 
         if key == ord('s')and save:
             ut.save_session(models_path)
 
-        # take an image to make a model of it
+        # take an image to create a model
         if key == ord('m'):
             save = True
             md = cv.flip(frame, 1)
@@ -40,12 +45,42 @@ def play(dev=0):
             model_index+=1
 
         # make the matching with the image and the models
-        if key == ord('c') and load:
-            capt = cv.flip(frame, 1)
-            # do the matching here
-            
+        if key == ord('c') and matching:
+            models, ind = ut.load_session(models_path)
 
-        cv.imshow('frame', frame)
+            capt = cv.flip(frame, 1)
+            md = models[1]
+            kpoints1, ds1 = method.detectAndCompute(capt, None)
+            #kpoints2, ds2 = method.detectAndCompute(md, None)
+#####
+            good = []
+            model = None
+            for md in models:
+                kpoints2, ds2 = method.detectAndCompute(md, None)
+                matches = bf.knnMatch(ds1, ds2, k = 2)
+
+                temp = []
+                for m,n in matches:
+                    if m.distance < 0.8*n.distance:
+                        temp.append(m)
+                if len(temp) > len(good):
+                    good = temp
+                    model = md
+#####
+            #matches = bf.knnMatch(ds1, ds2, k = 2)
+
+            #good = []
+            #for m,n in matches:
+            #    if m.distance < 0.8*n.distance:
+            #        good.append(m)
+            #print(len(good))
+
+            # don't draw single points
+            #sol = cv.drawMatchesKnn(capt, kpoints1, md, kpoints2, matches[:15], None, flags = 2)
+            sol = cv.drawMatchesKnn(capt, kpoints1, model, kpoints2, matches, None, flags = 2)
+            cv.imshow('sol', sol)
+
+        cv.imshow('frame', cv.flip(frame, 1))
 
         if key == 27:
             break
